4ead41c6a7e9d1f57410f53a5b46b6d769bbbf36
dinhtu <dinhtu444@gmail.com>
2021-08-10 17:06:42 +0700

    fix home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -13,6 +14,9 @@ class HomeController extends Controller
      */
     public function index()
     {
+        if (Auth::check()) {
+            return redirect(route('producer.dashboard.index'));
+        }
         return view('home.index', [
             'title' => 'ホーム',
             'showNav' => true
